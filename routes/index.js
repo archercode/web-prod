0be126bf9bfa647c98de302bdbc494f6812f5559
archercode <tonnyquintos@gmail.com>
2015-05-11 02:48:14 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,20 @@
  */
 
 exports.index = function(req, res){
+	//return;
   res.render('index');
+};
+
+exports.boards = function(req, res){
+	return;
+  //res.locals.session = req.session;
+	res.render('boards');
+};
+
+exports.sensors = function(req, res){
+  res.render('sensors');
+};
+
+exports.robotics = function(req, res){
+  res.render('robotics');
 };
